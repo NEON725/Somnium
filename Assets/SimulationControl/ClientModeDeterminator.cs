f9c12a12747e896d65f313cf567235259d5a8847
NEON725 <NEON725@hotmail.com>
2020-08-23 14:30:33 -0500

    Added ClientModeDeterminator.

---
+++ Assets/SimulationControl/ClientModeDeterminator.cs
@@ -0,0 +1,60 @@
+using System;
+using Unity.Entities;
+using UnityEngine;
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public class ClientModeDeterminator:ComponentSystem
+{
+	public static bool isClient{get;protected set;}
+	public static bool isServer{get;protected set;}
+
+	protected override void OnUpdate()
+	{
+		isServer=Application.isBatchMode;
+		isClient=!isServer;
+		#if UNITY_EDITOR
+			isServer=true;
+		#endif
+		if(!isClient)
+		{
+			World.GetExistingSystem<PresentationSystemGroup>().Enabled=false;
+		}
+		Enabled=false;
+	}
+
+	public void SetClientMode(bool mode)
+	{
+		isClient=mode;
+		World.GetExistingSystem<PresentationSystemGroup>().Enabled=mode;
+	}
+	public void SetServerMode(bool mode)
+	{
+		isServer=mode;
+		if(mode)
+		{
+			World.GetExistingSystem<ClientNetworkingSystem>().Enabled=false;
+			try
+			{
+				World.GetExistingSystem<ServerNetworkingSystem>().Enabled=true;
+			}
+			catch(Exception e)
+			{
+				Debug.Log("Failed to initiate server networking.");
+				Debug.LogError(e);
+			}
+		}
+		else
+		{
+			World.GetExistingSystem<ServerNetworkingSystem>().Enabled=false;
+			try
+			{
+				World.GetExistingSystem<ClientNetworkingSystem>().Enabled=true;
+			}
+			catch(Exception e)
+			{
+				Debug.Log("Failed to initiate client networking.");
+				Debug.LogError(e);
+			}
+		}
+	}
+}
